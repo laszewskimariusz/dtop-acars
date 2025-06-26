b0e67664c4f61dadaeb35e5f7ce375a416400192
zatto <laszewskimariusz@gmail.com>
2025-06-26 23:36:30 +0100

    change to allowed hosts and dev mode on

--- topsky/topsky/settings.py
+++ topsky/topsky/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
