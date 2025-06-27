c2fad6060e85a245febffa62be2dfda957e84437
zatto <laszewskimariusz@gmail.com>
2025-06-27 22:37:36 +0100

    fix: add missing password_reset_complete URL alias

--- topsky/topsky/urls.py
+++ topsky/topsky/urls.py
@@ -30,6 +30,9 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', 
          auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
     
     path('', include('landing.urls')),
 ]
