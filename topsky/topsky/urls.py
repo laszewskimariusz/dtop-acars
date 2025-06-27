8968dc4d4445ca3bce25e01aa02e192e0239733e
zatto <laszewskimariusz@gmail.com>
2025-06-27 18:56:48 +0100

    Fix HttpResponse type error by specifying content_type as text/plain in home view

--- topsky/topsky/urls.py
+++ topsky/topsky/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, this is the homepage!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),  # <-- to obsługuje stronę główną
 ]
